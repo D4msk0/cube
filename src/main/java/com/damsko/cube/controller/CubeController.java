7703020c9ac69ebdf76cd063830070deb0c79d4d
Merry Damsko <m.damsko@jcourant.nl>
2024-01-12 16:41:31 +0100

    added some more geometric shapes together with Chad

--- src/main/java/com/damsko/cube/controller/CubeController.java
+++ src/main/java/com/damsko/cube/controller/CubeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/cube")
+@RequestMapping("/")
 public class CubeController {
     @GetMapping
     public String showCube() {
